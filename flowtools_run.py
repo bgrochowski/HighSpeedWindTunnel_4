c5e2e494344e2131bea516822b36dc57862fce5f
bgrochowski <90771203+bgrochowski@users.noreply.github.com>
2023-01-09 15:10:16 +0100

    yyyy dupa

--- flowtools_run.py
+++ flowtools_run.py
@@ -13,9 +13,14 @@ geom1file.close()
 
 xtab = []
 p__pt_measured_tab = []
+M_measured_tab = []
 
 h_tab = []
 Aratio_tab = []
+
+hk2 = 14.6
+dh__dx = 0.01692
+h0 = 8.0
 
 for line in testfile_lines:
     line = line.strip()
@@ -23,6 +28,7 @@ for line in testfile_lines:
         line = line.split()
         # xtab.append(float(line[0]))
         p__pt_measured_tab.append(float(line[1]))
+        M_measured_tab.append(flowtools.flowisentropic2(gamma, float(line[1]), 'pres')[0])
 
 for line in geom1file_lines:
     line = line.strip()
@@ -31,6 +37,8 @@ for line in geom1file_lines:
         xtab.append(float(line[0]))
         h_tab.append(float(line[1]))
         Aratio_tab.append(float(line[2]))
+
+
 
 p__pt_theoretical_tab = []
 M_theoretical_tab = []
@@ -41,10 +49,11 @@ for i in range(len(xtab)):
     else:
         regime = 'sup'
     values = flowtools.flowisentropic2(gamma, Aratio_tab[i], regime)
+    M_theoretical_tab.append(float(values[0]))
     p__pt_theoretical_tab.append(float(values[2]))
 
-plt.plot(xtab, p__pt_measured_tab[:25], 'bo-', label="Measured")
-plt.plot(xtab, p__pt_theoretical_tab, 'ro-', label="Theoretical")
+plt.plot(xtab, M_measured_tab[:25], 'bo-', label="Measured")
+plt.plot(xtab, M_theoretical_tab, 'ro-', label="Theoretical")
 plt.xlim(40, 200)
 plt.legend()
 plt.show()

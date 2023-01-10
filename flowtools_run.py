54167d6f4875818a7bd885c2ab9d067299dd6c76
bgrochowski <90771203+bgrochowski@users.noreply.github.com>
2023-01-10 15:13:35 +0100

    jazdaaaa

--- flowtools_run.py
+++ flowtools_run.py
@@ -1,34 +1,46 @@
 import flowtools
 import matplotlib.pyplot as plt
+from get_p_M_sub_not_choked import p_pt, M_local
 
 gamma = 1.4
 
-testfile = open('data_for_report/Data_Test_1AB.txt', 'r')
-testfile_lines = testfile.readlines()
-testfile.close()
+testfile1 = open('data_for_report/Data_Test_1AB.txt', 'r')
+testfile1_lines = testfile1.readlines()
+testfile1.close()
+
+testfile2 = open('data_for_report/Data_Test_2A.txt', 'r')
+testfile2_lines = testfile2.readlines()
+testfile2.close()
 
 geom1file = open('fixed_geom.txt', 'r')
 geom1file_lines = geom1file.readlines()
 geom1file.close()
 
 xtab = []
-p__pt_measured_tab = []
-M_measured_tab = []
+p__pt_measurement1_tab = []
+M_measurement1_tab = []
+
+p__pt_measurement2_tab = []
+M_measurement2_tab = []
 
 h_tab = []
 Aratio_tab = []
 
-hk2 = 14.6
-dh__dx = 0.01692
-h0 = 8.0
-
-for line in testfile_lines:
+for line in testfile1_lines:
     line = line.strip()
     if line[0]!="%":
         line = line.split()
         # xtab.append(float(line[0]))
-        p__pt_measured_tab.append(float(line[1]))
-        M_measured_tab.append(flowtools.flowisentropic2(gamma, float(line[1]), 'pres')[0])
+        p__pt_measurement1_tab.append(float(line[1]))
+        M_measurement1_tab.append(flowtools.flowisentropic2(gamma, float(line[1]), 'pres')[0])
+
+# for line in testfile1_lines:
+#     line = line.strip()
+#     if line[0]!="%":
+#         line = line.split()
+#         # xtab.append(float(line[0]))
+#         p__pt_measurement1_tab.append(float(line[1]))
+#         M_measurement1_tab.append(flowtools.flowisentropic2(gamma, float(line[1]), 'pres')[0])
 
 for line in geom1file_lines:
     line = line.strip()
@@ -50,8 +62,10 @@ for i in range(len(xtab)):
     M_theoretical_tab.append(float(values[0]))
     p__pt_theoretical_tab.append(float(values[2]))
 
-plt.plot(xtab, M_measured_tab[:25], 'bo-', label="Measured")
-plt.plot(xtab, M_theoretical_tab, 'r-', label="Theoretical")
+plt.plot(xtab, M_measurement1_tab[:25], 'bo-', label="Measured, measurement 1AB")
+plt.plot(xtab, M_theoretical_tab, 'r-', label="Theoretical measurement 1AB")
+plt.plot(xtab, M_measurement2_tab, 'go-', label="Measured measurement 2A")
+plt.plot(xtab, M_local, 'y-', label="Theoretical measurement 2A")
 plt.xlim(40, 200)
 plt.legend()
 plt.show()
